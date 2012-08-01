1135f8cfcff0be7029335ce31a5f58154378d071
David Dworin <dworin@gmail.com>
2012-08-01 09:41:27 -0400

    Updated need model

--- app/models/need.rb
+++ app/models/need.rb
@@ -1,4 +1,4 @@
 class Need < ActiveRecord::Base
-  attr_accessible :category_id, :latitude, :longitude, :needtext, :source
+  attr_accessible :category_id, :latitude, :longitude, :needtext, :source, :country, :state, :city
   belongs_to :category
 end
