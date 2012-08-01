8c717b99997754b5d195edbc922c67bc073ab72f
David Dworin <dworin@gmail.com>
2012-07-31 22:13:37 -0400

    Added in code to take location data from Twilio

--- app/controllers/recieve_text_controller.rb
+++ app/controllers/recieve_text_controller.rb
@@ -3,6 +3,10 @@ class RecieveTextController < ApplicationController
 		message_body = params["Body"]
 		from_number = params["From"]
 
+		from_city = params["FromCity"]
+		from_country = params["FromCountry"]
+		from_state = params["FromState"]
+
 		@need = Need.new(:source => from_number, :needtext => message_body, :category_id => 1)
 		@need.save
 
