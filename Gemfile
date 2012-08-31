a4fc88cd2aee0a21f06e8fba0430d2602b373916
David Dworin <dworin@gmail.com>
2012-08-31 10:29:55 -0400

    Added geocoding

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'https://rubygems.org'
 
 gem 'rails', '3.2.7'
 gem 'twilio-ruby'
+gem 'geocoder'
 
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
--- app/models/need.rb
+++ app/models/need.rb
@@ -1,4 +1,5 @@
 class Need < ActiveRecord::Base
   attr_accessible :category_id, :latitude, :longitude, :needtext, :source, :country, :state, :city
   belongs_to :category
+  geocoded_by :city + ',' :state + ',' + :country
 end
