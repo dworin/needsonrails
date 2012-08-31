564d5983964d94e759ee093bfd295fa7ee6326bb
David Dworin <dworin@gmail.com>
2012-08-31 10:58:37 -0400

    Fixed up the geocoder

--- app/models/need.rb
+++ app/models/need.rb
@@ -2,10 +2,10 @@ class Need < ActiveRecord::Base
   attr_accessible :category_id, :latitude, :longitude, :needtext, :source, :country, :state, :city
   belongs_to :category
   
-  def build_address
-  	self.address = "#{city}, #{state}, #{country}"
+  def address
+  	return "#{city}, #{state}, #{country}"
   end
-  
+
   geocoded_by :address
   after_validation :geocode
 end
