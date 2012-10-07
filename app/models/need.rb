6c2bed30fe230f860dd73123fa85b6157e4f866a
David Dworin <dworin@gmail.com>
2012-10-07 17:23:59 -0400

    Added an add to csv function

--- app/models/need.rb
+++ app/models/need.rb
@@ -9,3 +9,12 @@ class Need < ActiveRecord::Base
   geocoded_by :address
   after_validation :geocode
 end
+
+def self.to_csv
+	CSV.generate do |csv|
+		csv << column_names
+		all.each do |need|
+			csv << need.attributes.values_at(*column_names)
+		end
+	end
+end
